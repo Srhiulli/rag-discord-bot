07504f8a1943010908c8ab96d149f0da3067803f
srhiulli <srhiulli@gmail.com>
2025-07-27 12:33:21 -0300

    feat: add typescript, docker compose and OS Dashboard

--- src/opensearch.ts
+++ src/opensearch.ts
@@ -2,9 +2,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import { Client } from '@opensearch-project/opensearch';
+import fetch from 'node-fetch'; 
 
 export const client = new Client({
-  node: 'https://localhost:9200',
+  node: 'http://localhost:9200',
   auth: {
     username: 'admin',
     password: process.env.OPENSEARCH_INITIAL_ADMIN_PASSWORD || '',
@@ -19,8 +20,25 @@ export async function testConnection(): Promise<void> {
     const health = await client.cluster.health();
     console.log('OpenSearch status:', health.body.status);
   } catch (err) {
-    console.error('Erro ao conectar:', err);
+    console.error('Erro ao conectar OpenSearch:', err);
   }
 }
 
-testConnection()
+export async function testDashboards(): Promise<void> {
+  try {
+   const response = await fetch('http://localhost:5601', {
+  headers: {
+    Authorization:
+      'Basic ' + Buffer.from(`admin:${process.env.OPENSEARCH_INITIAL_ADMIN_PASSWORD}`).toString('base64'),
+  },
+});
+    console.log('Dashboards status:', response.status);
+  } catch (err) {
+    console.error('Erro ao conectar Dashboards:', err);
+  }
+}
+
+(async () => {
+  await testConnection();
+  await testDashboards();
+})();
